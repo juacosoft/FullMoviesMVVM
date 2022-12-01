7be03c87b9ca7b9176fbf0cbfa93dfe1345a031d
Joaquin Dev Android <joaquin.martinez@empowermentlabs.co>
2022-12-01 06:02:23 -0500

    initial commit

---
+++ app/src/main/java/com/juacodev/fullmovies/FullMoviesApp.kt
@@ -0,0 +1,7 @@
+package com.juacodev.fullmovies
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FullMoviesApp {
+}
---
+++ app/src/main/java/com/juacodev/fullmovies/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.juacodev.fullmovies.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juacodev.fullmovies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
